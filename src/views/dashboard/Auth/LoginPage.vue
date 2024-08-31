6ee5cc502768a6b88666db3625a8ee4a80be1c40
mohamednasra12 <mohamednasra638@gmail.com>
2024-08-31 00:44:07 -0800

    nasraupdate

--- src/views/dashboard/Auth/LoginPage.vue
+++ src/views/dashboard/Auth/LoginPage.vue
@@ -1,100 +1,35 @@
 <template>
-  <form action="">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-4" id="forgetPass">
-          <div ref="lottieContainer"></div>
-          <h2>New Here</h2>
-          <p>
-            How To recover Your Password and <br />
-            Reset it
-          </p>
-          <router-link to="/forget-password">
-            <button>Forget Password</button>
-          </router-link>
-        </div>
-
-        <div class="col-md-8">
-          <div class="login">
-            <h2>Login To Your Account</h2>
-            <input
-              type="email"
-              v-model="email"
-              placeholder="Enter Your Email or Phone"
-              class="form-control"
-            />
-            <div>
-              <input
-                :type="passwordType"
-                placeholder="Enter your password"
-                class="form-control"
-                v-model="password"
-              />
-              <i class="fa-solid fa-lock" @click="togglePasswordVisibility"></i>
-            </div>
-            <button @click="blockAlert" class="login-btn">دخول</button>
-          </div>
-        </div>
+  <div class="naim-login">
+    <form @submit.prevent>
+      <div class="logo">
+        <img src="../../../assets/photos/logo.png" alt="" />
       </div>
+      <label for="email">ادخل البريد الألكتروني</label>
+      <div class="input">
+        <input
+          type="email"
+          id="email"
+          v-model="email"
+          placeholder="ادخل البريد الألكتروني"
+          required
+        />
+      </div>
+      <label for="password">ادخل كلمة المرور</label>
+      <div class="input">
+        <input
+          type="password"
+          id="password"
+          v-model="password"
+          placeholder="ادخل كلمة المرور"
+          required
+        />
+      </div>
+      <button type="submit">تسجيل الدخول</button>
+    </form>
+    <div class="forget">
+      <router-link to="/forget-password">
+        <h6>هل نسيت كلمة المرور؟</h6>
+      </router-link>
     </div>
-  </form>
+  </div>
 </template>
-
-<script>
-import Swal from "sweetalert2";
-import lottie from "lottie-web";
-
-export default {
-  name: "LoginPage",
-  data() {
-    return {
-      email: "",
-      password: "",
-      passwordType: "password",
-    };
-  },
-  props: {
-    animationPath: {
-      type: String,
-      required: true,
-    },
-    loop: {
-      type: Boolean,
-      default: true,
-    },
-    autoplay: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  methods: {
-    togglePasswordVisibility() {
-      this.passwordType =
-        this.passwordType === "password" ? "text" : "password";
-    },
-    blockAlert() {
-      Swal.fire({
-        position: "top-end",
-        icon: "success",
-        title: "لقد تمت تسجيل الدخول بنجاح",
-        showConfirmButton: false,
-        timer: 1500,
-      });
-    },
-    loadAnimation() {
-      lottie.loadAnimation({
-        container: this.$refs.lottieContainer,
-        renderer: "svg",
-        loop: this.loop,
-        autoplay: this.autoplay,
-        path: this.animationPath,
-      });
-    },
-  },
-  mounted() {
-    this.loadAnimation();
-  },
-};
-</script>
-
-<style scoped></style>
