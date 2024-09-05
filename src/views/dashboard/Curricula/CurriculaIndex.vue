3a5cd61911213dbe9b185f33a7843c7c3657a2ec
Diaa-3adel <diaa3adel11@gmail.com>
2024-09-05 11:10:49 +0300

    diaa

--- src/views/dashboard/Curricula/CurriculaIndex.vue
+++ src/views/dashboard/Curricula/CurriculaIndex.vue
@@ -3,7 +3,7 @@
     <header-pages title="المناهج" button="+ اضافة منهج" link="/add-curricula" />
     <tables-page-vue
       :headers="tableHeaders"
-      :rows="tableRows"
+      :rows="tableRowsCurricula"
       :pages="tablePages"
       editLink="/edit-curricula"
       viewLink="/view-curricula"
@@ -14,33 +14,34 @@
 <script>
 import HeaderPages from "@/components/headerpages/HeaderPages.vue";
 import TablesPageVue from "@/components/tables/TablesPage.vue";
+import { useCurriculaStore } from "@/stores/curricula/curriculaStore";
+import { mapState } from "pinia";
 export default {
   components: { HeaderPages, TablesPageVue },
   data() {
     return {
       tableHeaders: ["ID", "الصور", "اسم المنهج", "وصف المنهج"],
-      tableRows: [
-        [
-          "1",
-          require("@/assets/photos/Rectangle 8917.png"),
-          "المنهج",
-          " المنهج",
-        ],
-        [
-          "1",
-          require("@/assets/photos/Rectangle 8917.png"),
-          "المنهج",
-          "المنهج ",
-        ],
-        [
-          "1",
-          require("@/assets/photos/Rectangle 8917.png"),
-          "المنهج",
-          "المنهج ",
-        ],
-      ],
       tablePages: [1, 2, 3, 4, 5],
     };
+  },
+  computed: {
+    ...mapState(useCurriculaStore, {
+      Curriculas: (state) => state.Curriculas,
+      pages: (state) => state.pages,
+    }),
+
+    tableRowsCurricula() {
+      return this.Curriculas.map((cur) => [
+        cur.id,
+        cur.title,
+        cur.type,
+        cur.status,
+      ]);
+    },
+  },
+  mounted() {
+    const curriculaStore = useCurriculaStore();
+    curriculaStore.fetchCurricula();
   },
 };
 </script>
