b7d909c18b3f22f191226e43c6cb74cd41c6d96a
Diaa-3adel <diaa3adel11@gmail.com>
2024-09-17 16:19:57 +0300

    diaa

--- src/views/HomePage/HomeView.vue
+++ src/views/HomePage/HomeView.vue
@@ -8,8 +8,8 @@
 </template>
 
 <script>
-import HeaderView from "../../components/Home/HeaderView.vue";
-import BodyView from "../../components/Home/BodyView.vue";
+import HeaderView from "@/components/Home/HeaderView.vue";
+import BodyView from "@/components/Home/BodyView.vue";
 export default {
   name: "HomeView",
   components: { HeaderView, BodyView },
